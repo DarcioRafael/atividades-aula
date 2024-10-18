4198e74694bc89eb762a23bd8c2c70ffbccb09a0
DarcioRafael <liminhadarcioo@gmail.com>
2024-10-18 19:28:07 -0300

    Add files via upload

---
+++ lista aula.py
@@ -0,0 +1,214 @@
+#exercicio 1
+
+
+# n1 = int(input(' digite um numero: '))
+# if n1 >= 0:
+#     print(' o numero é positivo !')
+# elif n1 == 0:
+#     print(' o numero é 0')
+
+# else:
+#     print(' o é negativo !')       
+
+#exercicio 2
+# idade = int(input(' digite a idade: '))
+
+# if idade >= 18: 
+#     print(f' maior de idade !')
+# else:
+#     print(' menor de idade ! ')
+
+
+
+#exercicio 3
+
+# n1 = int(input(' digite o numero: '))
+# if n1 %2 ==0:
+#     print(' o numero é par')
+# else:
+#     print(' o numero é impar')
+
+#exercicio 4
+
+# n1 = int(input(' digite o primeiro numero: '))
+# n2 = int(input(' digite o segundo numero: '))
+# if n1 > n2:
+#     print(f' {n1} é maior que {n2}')
+# else:
+#     print(f'{n2} é maior que {n1}')
+
+#exercicio 5
+
+# valor = float(input(' digite o valor da compra: '))
+# desconto = valor * 10 /100
+# if valor >= 100:
+#     print(f'o valor da compra é:{valor - desconto}')
+# else:
+#     print(' voçé não ganhou desconto')
+
+#exercicio 6
+
+# n1 = int(input(' digite um numero: '))
+# if n1 % 5 == 0:
+#     print(f'{n1} é multiplo de 5')
+# else:
+#     print(f' {n1} não é multiplo de 5')
+
+#exercicio 7
+
+# nota1 = float(input(' digite a nota 1: '))
+# nota2 = float(input(' digite a nota 2: '))
+# nota3 = float(input(' digite a nota 3: '))
+# media = ( nota1 + nota2 + nota3) / 3 
+
+# if media >= 7:
+#     print(' aluno aprovado')
+# else:
+#     print(' aluno reprovado')
+
+#exercicio 8
+
+# senha = input(' digite a senha: ')
+# senha_correta = 'python123_EFG'
+# if senha == senha_correta:
+#     print(' Acesso permitido ')
+# else:
+#     print('Senha incorreta !')
+
+
+#exercicio 9
+
+# idade = int(input(' digite sua iadade:'))
+# if idade <= 5:
+#     print(' entrada gratuita ')
+# elif idade >= 60:
+#     print(' entrada gratuita ')
+# else:
+#     print(' não tem direito a entrada gratuita')
+
+#exercicio 10
+
+# nota = float(input(' digite uma nota entre 0 e 10: '))
+# if nota < 0 or nota > 10:
+#     print(' erro, digite uma nopta entre 0 e 10 !')
+
+# exercicio 11
+
+# idade = int(input(' digite a idade: '))
+# if idade >= 12 and idade <= 17:
+#     print(' adolescente')
+# elif idade >= 18:
+#     print(' adulto ')
+# else:
+#     print(' criança')
+
+#exercicio 12
+
+# numero1 = int(input('digite o primeiro numero: '))
+# numero2 = int(input('digite o segundo numero: '))
+# numero3 = int(input('digite o terceiro numero:' ))
+
+# maior = numero1
+# if numero2 > maior and numero2 > numero3:
+#  maior = numero2
+# if numero3 > maior and numero3 > numero2:
+#  maior = numero3
+
+# menor = numero1
+# if numero2 < menor and numero2 < numero3:
+#    menor = numero2
+# if numero3 < menor and numero3 < numero2:
+#      menor = numero3
+# print(f'o maior numero é {maior}')
+
+# exercicio 13
+
+# n1 = float(input(' digite o primeiro numero: '))
+# n2 = float(input(' digite o segundo numero: '))
+# if n2 == 0:
+#     print('a divisão não é possivel ')
+# else:
+#     print(f'{n1} / {n2} = {n1 / n2}')
+
+#exercicio 14
+
+# n1 = int(input(' digite um numero: '))
+# if n1 >=10 and n1 <=50:
+#     print(f' o numero: {n1} esta entre 10 e 50 ')
+# else:
+#     print(f' o numero: {n1} não esta entre 10 e 50 ')
+
+# exercicio 15
+
+# media = float(input(' digie a media: '))
+# if media >= 7:
+#     print(' Aprovado ')
+# elif media >=5 and media <7:
+#     print(' Em Recuperação ')
+# else:
+#     print(' Reprovado ')
+
+#exercicio 16 
+
+# peso = float(input(' digite o peso: '))
+# altura = float(input(' digite a altura: '))
+# imc = peso / altura ** 2
+# print(f' o IMC é: {imc}')
+
+# if imc < 18.5:
+#     print(' abaixo do peso ')
+# elif imc >= 18.5 and imc < 25:
+#     print(' peso normal ')
+# else:
+#     print(' acima do peso ')
+
+#exercicio 17
+# lado_1 = float(input('informe um lado: '))
+# lado_2 = float(input('infotme segundo lado: '))
+# lado_3 = float(input('informe terçeiro lado: '))
+
+# if lado_1 > (lado_2 + lado_3) or lado_2 > (lado_1 + lado_3) or lado_3 > (lado_1 + lado_2):
+#  print('não pode ser triangulo')
+# elif lado_1 == lado_2 == lado_3:
+#  print('Equilatero')
+# elif lado_1 == lado_2 or lado_1 == lado_3 or lado_2 == lado_3:
+#  print('isosceles')
+# else:
+#  print('Escaleno')
+
+ 
+
+# exercicio 18
+
+# usuario = input(' digite o nome de usuario: ')
+# senha = int(input(' digite a senha: '))
+
+
+# if usuario == 'admin' and senha == 1234:
+#     print(' Login bem sucedidio ')
+# else:
+#     print(' usuario ou senha incorretos ')
+
+# exercicio 19
+
+# idade = int(input(' digite sua iadade: '))
+# ano = 18 - idade
+
+# if idade >= 18:
+#     print(' pode dirigir ')
+# else:
+#     print(f' ainda nãp pode dirigir faltam, {ano} anos ')
+
+
+
+ 
+    
+
+
+
+
+
+
+
+    
+    
